c7c9e1590a4f695c92b7693f71d816e4fcc5fb21
Soumadeep360 <soumadeepsarkar4@gmail.com>
2024-12-15 12:24:54 +0530

    updated

--- 4.c
+++ 4.c
@@ -1,95 +1,68 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 #define SIZE 16
 
-int array[SIZE] = {38, 27, 43, 3, 9, 82, 10, 29, 42, 2, 1, 8, 5, 6, 7, 4};
-
-typedef struct
-{
-    int left;
-    int right;
-} thread_data;
+int a[SIZE] = {38, 27, 43, 3, 9, 82, 10, 29, 42, 2, 1, 8, 5, 6, 7, 4};
 
 void merge(int low, int mid, int high)
 {
+    int *temp = (int *)malloc((high - low + 1) * sizeof(int));
     int left = low;
     int right = mid + 1;
-    int size = high - low + 1;
-    int *temp = (int *)malloc(size * sizeof(int));
     int k = 0;
-
     while (left <= mid && right <= high)
     {
-        if (array[left] <= array[right])
+        if (a[left] < a[right])
         {
-            temp[k++] = array[left++];
+            temp[k++] = a[left++];
         }
         else
         {
-            temp[k++] = array[right++];
+            temp[k++] = a[right++];
         }
     }
-
     while (left <= mid)
-    {
-        temp[k++] = array[left++];
-    }
-
+        temp[k++] = a[left++];
     while (right <= high)
+        temp[k++] = a[right++];
+    for (int i = 0; i < high - low + 1; i++)
     {
-        temp[k++] = array[right++];
+        a[i + low] = temp[i];
     }
-
-    for (int i = 0; i < size; i++)
-    {
-        array[low + i] = temp[i];
-    }
-
     free(temp);
 }
 
 void *merge_sort(void *arg)
 {
-    thread_data *data = (thread_data *)arg;
-    int left = data->left;
-    int right = data->right;
-
-    if (left < right)
+    int *ind = (int *)arg;
+    int low = ind[0];
+    int high = ind[1];
+    if (low < high)
     {
-        int mid = left + (right - left) / 2;
-
-        thread_data left_data = {left, mid};
-        thread_data right_data = {mid + 1, right};
-
+        int mid = low + (high - low) / 2;
+        int left_ind[2] = {low, mid};
+        int right_ind[2] = {mid + 1, high};
         pthread_t left_thread, right_thread;
-        pthread_create(&left_thread, NULL, merge_sort, &left_data);
-        pthread_create(&right_thread, NULL, merge_sort, &right_data);
-
+        pthread_create(&left_thread, NULL, merge_sort, left_ind);
+        pthread_create(&right_thread, NULL, merge_sort, right_ind);
         pthread_join(left_thread, NULL);
         pthread_join(right_thread, NULL);
 
-        merge(left, mid, right);
+        merge(low, mid, high);
     }
-
-    return NULL;
 }
 
 int main()
 {
     pthread_t thread;
-    thread_data initial_data = {0, SIZE - 1};
-
-    pthread_create(&thread, NULL, merge_sort, &initial_data);
+    int ind[2] = {0, SIZE - 1};
+    pthread_create(&thread, NULL, merge_sort, ind);
     pthread_join(thread, NULL);
-
-    printf("Sorted Array:\n");
     for (int i = 0; i < SIZE; i++)
     {
-        printf("%d ", array[i]);
+        printf("%d ", a[i]);
     }
     printf("\n");
-
-    return 0;
 }
